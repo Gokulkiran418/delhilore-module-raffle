9e86fa1b99602e02899d16e4c40ddcffc2a4b0e3
Gokulkiran418 <umgokulkiran@gmail.com>
2025-07-09 12:23:43 +0530

    Edit success_url and cancel_url

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -18,8 +18,8 @@ export default async function handler(req, res) {
           quantity: 1,
         }],
         mode: 'payment',
-        success_url: 'http://localhost:3000/?success=true',
-        cancel_url: 'http://localhost:3000/?success=false',
+        success_url: 'http://delhilore.vercel.app/?success=true',
+        cancel_url: 'http://delhilore.vercel.app/?success=false',
       });
       res.status(200).json({ sessionId: session.id });
     } catch (error) {
